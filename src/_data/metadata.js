843862563496f3a723821a543d51fb734847bf7c
Snakie <snakie.ho@gmail.com>
2023-11-23 20:51:19 +0800

    add favicon & update title

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-	title: "Snakie002 何師傅",
+	title: "何師傅遊戲攻略",
 	url: "https://snakie002hosifu.blog/",
 	language: "zh-Hant",
 	description: "何師傅 遊戲攻略 - POE 暗黑破壞神 火炬之光無限 攻略教學 2023",
