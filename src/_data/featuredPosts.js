640bad2f6389e9693e126afbbb67cd4fbbaa48b9
snakie002 <snakie.ho@gmail.com>
2024-10-06 01:42:35 +1000

    first commit

--- src/_data/featuredPosts.js
+++ src/_data/featuredPosts.js
@@ -1,34 +1,10 @@
 module.exports = {
 	posts: [
 		{
-			slug: "3-22-guardian-skelly-mage",
+			slug: "20240914",
 		},
 		{
-			slug: "poe-3-22-totaguide",
-		},
-		{
-			slug: "3-22-boneshatter",
-		},
-		{
-			slug: "3-22-leaguestart-lightningarrow",
-		},
-		{
-			slug: "3-22-leaguestart-colddot",
-		},
-		{
-			slug: "3-22-leaguestart-boneshatter",
-		},
-		{
-			slug: "3-22-leaguestart",
-		},
-		{
-			slug: "poe-3-22-0815",
-		},
-		{
-			slug: "3-22-patch",
-		},
-		{
-			slug: "poe2-demo",
+			slug: "20240914-2",
 		},
 	],
 };
